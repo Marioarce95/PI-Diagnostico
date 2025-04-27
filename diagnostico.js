610e0fec3353522681ecb5daf9f892484c1ecf0d
Mario Arce <marioezequielarce95@gmail.com>
2025-04-27 15:39:02 -0300

    Agregado archivo inicial diagnostico.js

---
+++ diagnostico.js
@@ -0,0 +1,13 @@
+let nombre;
+let edad;
+
+nombre= "Ana";
+edad= "22";
+
+console.log(`Hola soy ${nombre} y tengo ${edad} años.`)
+
+const numeros = [3, 7, 12, 5, 2];
+
+const cuadrados = numeros.map((numero) => numero * numero);
+
+console.log(cuadrados);
